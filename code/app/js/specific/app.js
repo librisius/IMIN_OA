b5893de11d258dc34ce4870fa4a157b7e8e82144
librisius <fugaon@mail.ru>
2018-04-20 18:21:00 +0300

    final desktop

--- code/app/js/specific/app.js
+++ code/app/js/specific/app.js
@@ -4,8 +4,34 @@
 
 		(function() {
 
-			if ( $('.c-block').length ) {
+			if ( $('.c-accordion').length ) {
 
+				$('.c-accordion').each( function( index, el ) {
+
+					var $this				= $( this ),
+						$item				= $this.find( '.c-accordion__item' ),
+						$bind				= $this.find( '.c-accordion__bind' ),
+						className			= $(this).data( 'c-accordion-class' );
+
+					classActive( $item.eq(2) );
+
+					$bind.click( function( event ) {
+
+						classActive( $( this ).closest( $item ) );
+					});
+
+					function classActive( $element ) {
+
+						if ( !$element.hasClass( className ) ) {
+
+							$element.addClass( className ).siblings().removeClass( className );
+
+						} else {
+
+							$element.removeClass( className );
+						}
+					}
+				});
 			}
 		})();
 	});
